575d33ec76098edd9d5d5cf1ec46f3419aed34b2
ckb3388 <118872644+ckb3388@users.noreply.github.com>
2023-10-04 00:24:42 +0900

    Delete backup/stew/myhome directory

--- backup/stew/myhome/pybo/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import StoryDb
-
-# Register your models here.
-class StoryDbAdmin(admin.ModelAdmin):
-    search_fields = ['content']
-
-admin.site.register(StoryDb, StoryDbAdmin)
-
