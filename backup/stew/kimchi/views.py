4a236a6a840831c5d0c71464b753ffa1c52fc2e9
southsea <86471807+southsea3026@users.noreply.github.com>
2023-10-07 20:15:10 +0900

    Add files via upload

--- backup/stew/kimchi/views.py
+++ backup/stew/kimchi/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import textStory
 
 def page_not_found(request, exception):
     return render(request, '404.html', {})
 
 def index(request):
+    if not request.session.get('visited', False):
+        request.session['visited'] = True
+        request.session['current_story_index'] = 0
+        request.session['score'] = 0
+
     story_list = textStory.objects.order_by('-create_date')
     context = {'story_list': story_list}
     return render(request, 'kimchi/index.html', context)
 
 def textplay(request):
+
     if request.method == 'POST':
         choice = request.POST.get('choice')
         current_story_index = request.session.get('current_story_index', 0)
